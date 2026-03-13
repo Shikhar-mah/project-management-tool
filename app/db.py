33c37b99e5b18ad94e867864d0ebeab204ea33e9
Shikhar <shikharm.3110@gmail.com>
2026-03-13 12:32:41 +0530

    general

--- app/db.py
+++ app/db.py
@@ -1,38 +1,6 @@
-import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from collections.abc import AsyncGenerator
-import uuid
-from dotenv import load_dotenv
 
-from sqlalchemy import Column, String, Text, DateTime, ForeignKey
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, relationship
-
-db_username = os.getenv("DATABASE_USERNAME")
-db_password = os.getenv("DATABASE_PASSWORD")
-
-DATABASE_URL = "sqlite+aisqlite:///./test.db"
-
-class Post(DeclarativeBase):
-    __tablename__ = "posts"
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    caption = Column(Text)
-    url = Column(String, nullable=False)
-    file_type = Column(String, nullable=False)
-    file_name = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.time)
-
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
-
-async def create_db_and_table():
-    async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
-
-async def get_async_session()-> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
-
-
+# Database URL
+DATABASE_URL = "postgresql://postgres:password@localhost:5432/mydatabase"
